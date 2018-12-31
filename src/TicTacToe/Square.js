19852a2bdb66ab6b964409582cf599691eac90b3
JeffBraches <jeff.braches@gmail.com>
2018-12-30 21:24:42 -0500

    TicTacToe Square CSS fixes

--- src/TicTacToe/Square.js
+++ src/TicTacToe/Square.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Square = ({value, handleSelection, location}) =>{
     return(
-        <button className="square" onClick={() => handleSelection(location)}>{value}</button>
+        <button className="square" onClick={() => handleSelection(location)}>
+            <h3 className="squareText">{value}</h3>
+        </button>
     )
 }
 
