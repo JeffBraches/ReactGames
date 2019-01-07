a41d52b40016e8d872d0ce19c6723686de7cc0d7
Jeffrey Braches <jeffrey.braches@fdmgroup.com>
2019-01-07 18:22:06 -0500

    Updates

---
+++ precache-manifest.2b789f9eb315eb0269fa7755cf7f886c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9056f54db110b04b9e93",
+    "url": "/ReactGames/static/js/runtime~main.9056f54d.js"
+  },
+  {
+    "revision": "dd47a96e7850ed81b32a",
+    "url": "/ReactGames/static/js/main.dd47a96e.chunk.js"
+  },
+  {
+    "revision": "84039e4302c30df0a09f",
+    "url": "/ReactGames/static/js/1.84039e43.chunk.js"
+  },
+  {
+    "revision": "dd47a96e7850ed81b32a",
+    "url": "/ReactGames/static/css/main.43acd610.chunk.css"
+  },
+  {
+    "revision": "f440db46eb1df3d79339c7a779a30dcd",
+    "url": "/ReactGames/index.html"
+  }
+];
